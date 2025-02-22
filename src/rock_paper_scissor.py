fc7a8329e2a836a01b4344d83fca6940033e1dfe
sudoCreate258 <59755293+sudoCreate258@users.noreply.github.com>
2025-02-21 20:47:58 -0500

    Create rock_paper_scissor.py
    init

---
+++ src/rock_paper_scissor.py
@@ -0,0 +1,18 @@
+#!/bin/bash/python3
+import random  
+
+def main():
+    # ** comp value may be [1,2,3] at 'random'
+    comp_value = random.randint(1,3) 
+    #assume rock/paper/scissor to represent 0/1/2 or alt.
+
+    #TODO: prompt the user to enter a value in 1-3 (as rock, paper, scissors)
+    #TODO: input user value into a variable (condider data types)
+    #TODO: utilize conditionals to perform input validation on user input
+    
+    #TODO utilize a relational expression to verify if it's a tie
+    #TODO utilize nested/chained conditional or match case for a winner
+    #recall rock beats scissors, scissors beats paper, paper beats rock
+
+if __name__ == "__main__":
+    main()
